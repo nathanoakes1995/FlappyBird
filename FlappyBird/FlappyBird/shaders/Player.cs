f2e5c1fcae2f9f90bb9a389cf867568a77e4d994
unknown <o014892c@k106-19.student.staffs.ac.uk>
2014-10-02 10:43:06 +0100

    Recreated Player Class

---
+++ FlappyBird/FlappyBird/shaders/Player.cs
@@ -0,0 +1,39 @@
+using System;
+struct Vector2
+{
+	int x, y;
+};
+
+namespace FlappyBird
+{
+	public class Player
+	{
+		public Player ()
+		{
+			Vector2 position = new Vector2(50,50);
+			
+		}
+		
+		public void LoadContent()
+		{
+			
+			
+		}
+		
+		public void Update()
+		{
+			
+		}
+		
+		public void Movement()
+		{
+			
+		}
+		
+		public void Draw()
+		{
+			
+		}
+	}
+}
+
