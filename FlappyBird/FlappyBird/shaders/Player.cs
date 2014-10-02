e9abc79c449dd763e2697f220765dc1fa5a119cd
unknown <o014892c@k106-19.student.staffs.ac.uk>
2014-10-02 10:31:27 +0100

    [+] Player Class[NO]

---
+++ FlappyBird/FlappyBird/shaders/Player.cs
@@ -0,0 +1,37 @@
+using System;
+struct Vector2
+{
+	int x, y;
+};
+
+namespace FlappyBird
+{
+	public class Player
+	{
+		public Player ()
+		{
+		}
+		
+		public void LoadContent()
+		{
+			Vector2 position;
+			position = new Vector2(50,50);
+		}
+		
+		public void Update()
+		{
+			
+		}
+		
+		public void Movement()
+		{
+			
+		}
+		
+		public void Draw()
+		{
+			
+		}
+	}
+}
+
